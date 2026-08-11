8a5e5a76b950b2ed4cfdfe9142b472b509095ae6
LuckysHorizon <fruitylucky@gmail.com>
2026-08-11 12:38:11 +0530

    test: restore Testcontainers PostgreSQL integration tests

--- backend/src/test/java/com/travelbillpro/BackendApplicationTests.java
+++ backend/src/test/java/com/travelbillpro/BackendApplicationTests.java
@@ -2,12 +2,36 @@ package com.travelbillpro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry reg) {
+        reg.add("spring.datasource.url", postgres::getJdbcUrl);
+        reg.add("spring.datasource.username", postgres::getUsername);
+        reg.add("spring.datasource.password", postgres::getPassword);
+        reg.add("spring.flyway.url", postgres::getJdbcUrl);
+        reg.add("spring.flyway.user", postgres::getUsername);
+        reg.add("spring.flyway.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
